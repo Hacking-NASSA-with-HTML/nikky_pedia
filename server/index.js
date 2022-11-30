30990cb610e9a7fab02ea3381465d60ff4efc528
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2022-11-30 18:00:25 +0400

    feat: add login auth

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js"
 import { register } from "./controllers/auth.js";
 
 
@@ -40,6 +41,9 @@ const upload = multer({ storage })
 
 /* Routes with files  */
 app.post("/auth/register", upload.single("picture"), register)
+
+/* Routes */
+app.use("/auth", authRoutes)
 
 
 /* Mongoose setup  */
