1609635d8b6a2d03f0cafc57712a296994755238
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2022-11-30 15:28:22 +0400

    feat: add multer file storage configuration

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 
+
 /* Configurations */
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -22,3 +23,14 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors())
 app.use("/assets", express.static(path.join(__dirname, "public/assets")))
+
+
+/* File Storage */
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "public/assets")
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+})
