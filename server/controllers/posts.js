4649a6a8e97059fb5994320d2a815349efa47636
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2022-12-20 11:49:55 +0400

    feat: add sorting newest posts to display first

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -19,7 +19,7 @@ export const createPost = async (req, res) => {
         })
         await newPost.save()
 
-        const post = await Post.find()
+        const post = await Post.find().sort({ createdAt: -1 })
         res.status(201).json(post)
     } catch (err) {
         res.status(409).json({ message: err.message })
@@ -29,7 +29,7 @@ export const createPost = async (req, res) => {
 /* READ */
 export const getFeedPosts = async (req, res) => {
     try {
-        const post = await Post.find()
+        const post = await Post.find().sort({ createdAt: -1 })
         res.status(200).json(post)
     } catch (error) {
         res.status(404).json({ message: error.message })
@@ -39,7 +39,7 @@ export const getFeedPosts = async (req, res) => {
 export const getUserPosts = async (req, res) => {
     try {
         const { userId } = req.params
-        const post = await Post.find({ userId })
+        const post = await Post.find({ userId }).sort({ createdAt: -1 })
         res.status(200).json(post)
     } catch (error) {
         res.status(404).json({ message: error.message })
